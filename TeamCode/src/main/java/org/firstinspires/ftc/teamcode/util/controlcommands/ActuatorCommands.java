5e4728a5ee3f8f85dc75eac6a5a11a04f85f14ae
ImBonana <eladtairo@gmail.com>
2025-09-05 14:45:56 +0300

    Add ActuatorCommands & DriverCommands classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/controlcommands/ActuatorCommands.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util.controlcommands;
+
+import org.firstinspires.ftc.teamcode.subsystems.driveTrain.MecanumDriveSubsystem;
+
+public class ActuatorCommands {
+    private final MecanumDriveSubsystem mecanumDriveSubsystem;
+
+    public ActuatorCommands(MecanumDriveSubsystem mecanumDriveSubsystem) {
+        this.mecanumDriveSubsystem = mecanumDriveSubsystem;
+    }
+
+    // Commands here /ᐠ｡ꞈ｡ᐟ\
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/controlcommands/DriverCommands.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util.controlcommands;
+
+import org.firstinspires.ftc.teamcode.subsystems.driveTrain.MecanumDriveSubsystem;
+
+public class DriverCommands {
+    private final MecanumDriveSubsystem mecanumDriveSubsystem;
+
+    public DriverCommands(MecanumDriveSubsystem mecanumDriveSubsystem) {
+        this.mecanumDriveSubsystem = mecanumDriveSubsystem;
+    }
+
+    // Commands here ✍️(◔◡◔)
+}
