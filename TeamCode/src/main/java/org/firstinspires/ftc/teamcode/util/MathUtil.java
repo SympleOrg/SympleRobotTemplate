5f5ba92655f6605e479dc6b0596a284a9b7f9d9f
ImBonana <eladtairo@gmail.com>
2025-09-05 15:44:51 +0300

    Add docs to util classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MathUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MathUtil.java
@@ -1,14 +1,41 @@
 package org.firstinspires.ftc.teamcode.util;
 
 public class MathUtil {
+    /**
+     * Converts encoder ticks to degrees of rotation.
+     *
+     * @param ticks    the number of encoder ticks
+     * @param maxTicks the total number of ticks per full rotation (360°)
+     * @return the corresponding angle in degrees
+     */
     public static double ticksToDeg(int ticks, double maxTicks) {
         return ((ticks * 360) / maxTicks);
     }
 
+
+    /**
+     * Converts an angle in degrees to encoder ticks.
+     *
+     * @param deg      the angle in degrees
+     * @param maxTicks the total number of ticks per full rotation (360°)
+     * @return the corresponding encoder tick count
+     */
     public static double degToTicks(double deg, double maxTicks) {
         return ((deg * maxTicks) / 360.0f);
     }
 
+    /**
+     * Converts encoder ticks into linear distance traveled by a wheel.
+     *
+     * <p>The formula accounts for wheel circumference and gear ratio.</p>
+     *
+     * @param ticks       the number of encoder ticks
+     * @param wheelRadius the radius of the wheel (in meters)
+     * @param ticksPerRev the number of encoder ticks per full revolution
+     * @param gearRatio   the gear ratio (output revolutions / input revolutions).
+     *                    Use 1.0 if the encoder is mounted directly on the wheel shaft.
+     * @return the distance traveled in meters
+     */
     public static double encoderTicksToMeter(double ticks, double wheelRadius, double ticksPerRev, double gearRatio) {
         return (Math.PI * 2 * wheelRadius) / (ticksPerRev * gearRatio) * ticks;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TeamColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TeamColor.java
@@ -1,6 +1,16 @@
 package org.firstinspires.ftc.teamcode.util;
 
+/**
+ * Represents the two possible alliance colors in a match.
+ */
 public enum TeamColor {
+    /**
+     * The red alliance.
+     */
     RED,
+
+    /**
+     * The blue alliance.
+     */
     BLUE
 }
